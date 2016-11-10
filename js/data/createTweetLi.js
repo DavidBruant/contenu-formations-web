f1c481e36784a9761d6f2bef34f31b4861a57669
simon-milleto <contact@simonmilleto.com>
2016-11-10 14:34:27 +0100

    message

--- js/data/createTweetLi.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function createTweetLi(tweet){
-  var li = document.createElement('li');
-  li.textContent = tweet.text;
-  return li;
-}
--- js/data/createTweetOl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function createTweetOl(tweets, lang){
-  var ol = document.createElement('ol');
-  tweets.forEach(function(element){
-    ol.appendChild(createTweetLi(element));
-  });
-  return ol;
-}
