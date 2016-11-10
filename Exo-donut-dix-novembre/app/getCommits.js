579df93a474c188cdd76d51e26e757a43514a3a2
Clément Ollivier <clement.o2p@gmail.com>
2016-11-10 14:36:04 +0100

    Added github token

--- Exo-donut-dix-novembre/app/getCommits.js
+++ Exo-donut-dix-novembre/app/getCommits.js
@@ -1,7 +1,9 @@
 export default function getCommits(){
-	const commits = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits')
+	const commits = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=1d1ea5599fad961bab88d7fddeafb9a7fb69e0cd')
 					.then(resp => {
 						return resp.json();
 					});
 	return commits
 };
+
+// 1d1ea5599fad961bab88d7fddeafb9a7fb69e0cd
--- Exo-donut-dix-novembre/app/main.js
+++ Exo-donut-dix-novembre/app/main.js
@@ -7,7 +7,17 @@ document.addEventListener('DOMContentLoaded', () =>{
 	
 	getCommits()
 	.then(commits =>{
-		const commitsDate = commits.map(commit => moment(commit.commit.author.date).format('dddd'));
-		console.log(commitsDate);
+		// const commitsDate = commits.map(commit => moment(commit.commit.author.date).format('dddd'));
+		
+		// const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+		// weekDays.forEach(day =>{
+		// 	const dailyCommits = commitsDate.filter(commit {
+		// 		commit === day;
+		// 	});
+
+		// 	console.log(dailyCommits);
+
+		console.log(commits);
+		});
 	});
-});
