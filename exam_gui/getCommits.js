74e7cb4e678a54809082913f3b214aa3bd376382
Guillaume Darricau <g.darricau@hotmail.com>
2016-11-10 16:03:11 +0100

    Commit before pause

--- exam_gui/getCommits.js
+++ exam_gui/getCommits.js
@@ -1,8 +1,16 @@
 export default function getCommits(){
-    
-    var commits = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=34e29f79ffe4ea48acc3326403269b35a6de5f1b&page=2')
+
+    var commits = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=2522f2dffa38f134ebb034b56257c23e0a673669&page=1')
     .then(function(result){
         return result.json();
     })
-    return commits;
+
+    var commits2 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=2522f2dffa38f134ebb034b56257c23e0a673669&page=1')
+    .then(function(result){
+        return result.json();
+    })
+    
+    return Promise.all([commits, commits2]).then(function(result){
+        return result[0].concat(result[1]);
+    })
 }
--- exam_gui/main.js
+++ exam_gui/main.js
@@ -1,6 +1,7 @@
 "use strict";
 import getCommits from "./getCommits";
 import moment from "moment";
+import * as d3 from 'd3-shape'
 
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -46,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function(){
             }  
         })
         
-        
+        console.log(allTabs.monday);
         
     })
 })
