ae0d6ca9872c77bed77f06920e55e440f2c62ed9
Clément Ollivier <clement.o2p@gmail.com>
2016-11-10 14:51:47 +0100

    Console logging numbers of commits per day

--- Exo-donut-dix-novembre/app/main.js
+++ Exo-donut-dix-novembre/app/main.js
@@ -7,17 +7,18 @@ document.addEventListener('DOMContentLoaded', () =>{
 	
 	getCommits()
 	.then(commits =>{
-		// const commitsDate = commits.map(commit => moment(commit.commit.author.date).format('dddd'));
-		
-		// const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-		// weekDays.forEach(day =>{
-		// 	const dailyCommits = commitsDate.filter(commit {
-		// 		commit === day;
-		// 	});
-
-		// 	console.log(dailyCommits);
 
 		console.log(commits);
+		
+		const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+		weekDays.forEach(day =>{
+			
+			const dailyCommits = commits.filter(commit => moment(commit.commit.author.date).format('dddd') === day);
+
+			const dailyCommitsNumber = dailyCommits.length;
+
+			console.log(dailyCommitsNumber);
 		});
 	});
+});
