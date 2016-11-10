a6d8fd5c0debfeb6fb3a63c316d9a97ca128e108
Romain <romain.thoreau@laposte.net>
2016-11-10 18:13:41 +0100

    Il y a un truc à l'écran, c'est beau

--- donut/get-commits.js
+++ donut/get-commits.js
@@ -1,20 +1,20 @@
 export default function getCommits(){
 
-	var c1 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=2719b3599f1f63a7357aa764380544df39e12f2c&page=1&per_page=50')
+	var c1 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=6383432658b8e7a508f0eb1d78b0a9efec4b5d97&page=1&per_page=50')
 	.then(function(repo){
 		return repo.json();
 	})
 	.catch(function(error){
 		console.log(error);
 	});
-	var c2 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=2719b3599f1f63a7357aa764380544df39e12f2c&page=2&per_page=50')
+	var c2 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=6383432658b8e7a508f0eb1d78b0a9efec4b5d97&page=2&per_page=50')
 	.then(function(repo){
 		return repo.json();
 	})
 	.catch(function(error){
 		console.log(error);
 	});
-	var c3 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=2719b3599f1f63a7357aa764380544df39e12f2c&page=3&per_page=50')
+	var c3 = fetch('https://api.github.com/repos/Financial-Times/polyfill-service/commits?access_token=6383432658b8e7a508f0eb1d78b0a9efec4b5d97&page=3&per_page=50')
 	.then(function(repo){
 		return repo.json();
 	})
--- donut/make-donut.js
+++ donut/make-donut.js
@@ -5,28 +5,44 @@ export default function makeDonut(jourSemaine){
 	    height = 500,
 	    radius = Math.min(width, height) / 2;
 
-	var donut = document.createElement('svg');
+	var color = ["#0E4F7F", "#24597F", "#1683CC", "#208FCE", "#2DBEFF", "#CFE5FF", "#6ABFFF"]
+
+	var donut = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		donut.setAttribute("width", width);
 		donut.setAttribute("height", height);
-	var gGlobal = document.createElement('g');
+	var gGlobal = document.createElementNS('http://www.w3.org/2000/svg', 'g');
 	gGlobal.setAttribute("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+	var tableauData = new Array();
+
+	Object.keys(jourSemaine).forEach(function(jour){
+		tableauData.push(jourSemaine[jour]);
+	});
+
+	var data = d3.pie()(tableauData);
+	console.log(data);
+
+	var i=0;
+
     Object.keys(jourSemaine).forEach(function(jour){
-    	var g = document.createElement('g');
-    	g.className = 'arc';
-    	var path = document.createElement('path');
+    	console.log(data[i].startAngle);
+    	console.log(data[i].endAngle);
 
-    	var arc = d3.arc()
-	    .outerRadius(radius - 20)
-	    .innerRadius(radius - 70);
-	    path.setAttribute('d',arc);
-    	path.setAttribute('fill', 'blue');
+    	var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    	var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-    	var text = document.createElement('text');
+    	var arc = d3.arc()({outerRadius :(radius - 10),innerRadius :(radius - 70),startAngle :(data[i].startAngle),endAngle :(data[i].endAngle)});
+	    console.log(arc);
+
+	    path.setAttribute('d', arc);
+    	path.setAttribute('fill', color[i]);
+
+    	var text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     	text.textContent = jour;
     	g.appendChild(path);
     	g.appendChild(text);
     	gGlobal.appendChild(g);
+    	i++;
     });
 
     donut.appendChild(gGlobal);
